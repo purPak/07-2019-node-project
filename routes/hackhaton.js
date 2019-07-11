55749d21a5e549272b33d3a7ddb36fbfc0129d03
aeki <aekiwow@hotmail.fr>
2019-07-11 17:29:06 +0200

    redirection form

--- routes/hackhaton.js
+++ routes/hackhaton.js
@@ -1,8 +1,10 @@
 const express = require('express');
 var path = require('path');
+const bodyParser = require('body-parser');
 //Set up express router
 const router = express.Router();
 
+router.use(bodyParser.urlencoded({ extended: true }));
 //Route that show registration-form
 router.get('/',(req,res)=>{
     res.render('hackhaton/index.html');
@@ -19,9 +21,10 @@ router.get('/formulaire.js',(req,res)=>{
     res.sendFile(path.resolve('formulaire.js'));
     //res.send("formulaire");
 });
+
 //Route that add registration-form in db  
-router.post('/',(req,res)=>{
-    res.send("Post form");
+router.post('/hackhaton/confirmation',(req,res)=>{
+     res.render("hackhaton/confirmation.html", {name: req.body.name});
 });
 
 module.exports = router;
