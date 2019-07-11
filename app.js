b20bc5c2b94f66693a916e8ea7c59b5075ed07a5
purPak <yacinelahjaily@gmail.com>
2019-07-11 10:39:21 +0200

    Set Up Express server / Set Mongo DB connection

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+//Connect to Db
+mongoose.connect('mongodb://localhost:27017');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback () {
+  console.log("App is connected to Mongo DB");
+});
+
+//Set up Express server
+const app = new express();
+const port = 3000;
+const hostname = '127.0.0.1';
+
+//Set up routing 
+app.use('/', require('./routes/hackhaton.js'));
+
+//Connect to Express server
+app.listen(port,hostname, ()=> {
+    console.log(`Server running at http://${hostname}:${port}/`);
+})
+
